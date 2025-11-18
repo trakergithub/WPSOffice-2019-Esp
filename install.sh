133097ffeadb079d8218fe97c35329566a661198
Traker <trakerdroid@gmail.com>
2025-11-18 12:09:47 -0600

    Script parainstalar
    Instala WPS y agrega diccionarios y paquetes de idioma para la interfaz, ademas de corregir el error que queda durante la instalacion para abrir y exportar archivos PDF

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#Desinstala cualquier versión anterior instalada y elimina directorios
+sudo dpkg --purge --force-all wps-office 
+sudo dpkg --purge --force-all libwps-0.4-4:amd64
+sudo flatpak remove com.wps.Office
+sudo rm /opt/kingsoft -rf
+sudo rm /usr/share/fonts/wps-office -rf
+sudo rm ~/.local/share/Kingsoft/ -rf
+sudo rm /root/.local/share/Kingsoft/ -rf
+sudo apt autoclean
+clear
+
+#Instala la ultima versión de WPS al 18/11/2025 (app,y fuentes)
+sudo apt install wps-office_11.1.0.11723.XA_amd64.deb <<<'S'
+sudo apt install ttf-wps-fonts_1.0_all.deb <<<'S'
+rm ~/.local/share/Kingsoft/wps-office/office6/dicts/spellcheck -rf
+rm ~/.local/share/Kingsoft/wps-office/office6/mui -rf
+wps &
+wait
+
+#Copia las carpetas de diccionarios y de lenguaje a las carpetas de wps
+cp -a dicts/ ~/.local/share/Kingsoft/wps-office/office6/dicts/spellcheck
+cp -a mui/ ~/.local/share/Kingsoft/wps-office/office6/mui
+
+#Corregir error de exportar y abrir PDF
+sudo ln -s /usr/lib/x86_64-linux-gnu/libtiff.so.6 /usr/lib/x86_64-linux-gnu/libtiff.so.5
